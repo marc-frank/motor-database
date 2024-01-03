6e4ed3ce9e435b4719dbde625ac1ee5ff06eb2c4
Marc Frank <marc.silas.frank@gmail.com>
2024-01-03 15:25:16 +0100

    Create dataTableSetup.js

---
+++ dataTableSetup.js
@@ -0,0 +1,40 @@
+function setupDataTable(data) {
+    // Aggregate unique keys from all objects
+    var uniqueKeys = new Set();
+    data.forEach((item) => {
+        Object.keys(item).forEach((key) => {
+            uniqueKeys.add(key);
+        });
+    });
+
+    // Sort keys alphabetically, except for specific keys
+    var sortedKeys = Array.from(uniqueKeys).sort();
+    var startKeys = ["Image", "Manufacturer", "Name", "Price ($)", "Stator Diameter (mm)", "Stator Height (mm)"];
+    var endKey = "Link";
+    sortedKeys = startKeys.concat(sortedKeys.filter((key) => !startKeys.includes(key) && key !== endKey));
+    sortedKeys.push(endKey);
+
+    // Generate dynamic headers and footers
+    var headerFooterRow = "";
+    sortedKeys.forEach((key) => {
+        headerFooterRow += "<th>" + key + "</th>";
+    });
+    $("#myTable thead tr, #myTable tfoot tr").html(headerFooterRow);
+
+    // Create columns configuration
+    var columns = sortedKeys.map((key) => {
+        var columnDef = { data: key, defaultContent: "" };
+        if (key === "Image") {
+            columnDef.render = function (data) {
+                return data ? '<img src="' + data + '" alt="Image" width="128" height="128">' : "";
+            };
+        } else if (key === "Link") {
+            columnDef.render = function (data) {
+                return data ? '<a href="' + data + '" target="_blank">' + data + "</a>" : "";
+            };
+        }
+        return columnDef;
+    });
+
+    return columns;
+}
